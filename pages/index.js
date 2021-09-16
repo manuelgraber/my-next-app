333cfda342329075429427e7a99bcbbf546804b2
Manuel Graber <manuel@deep-impact.ch>
2021-09-16 18:59:21 +0200

    added text

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+	<h1>I am the DEV branch</h1>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
