76ac41764fd754e8ca7fbf558b822422199b7477
Manuel Graber <manuel@deep-impact.ch>
2021-09-17 15:55:48 +0200

    changed text

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+port Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-	<h1>I am the DEV branch</h1>
+	<h1>I am the DEV branch again</h1>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
